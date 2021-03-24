76cb021781cfb5c30f28975810ba30a7bb883584
fpeguero58 <49230091+fpeguero58@users.noreply.github.com>
2021-03-24 17:37:28 +0100

    Update Hola.java

--- ejercicioEclipse/src/ejercicioEclipse/Hola.java
+++ ejercicioEclipse/src/ejercicioEclipse/Hola.java
@@ -2,6 +2,6 @@ package ejercicioEclipse;
 
 public class Hola {
     public static void main(String[] args) {
-        System.out.println("Hola, ¿Qué tal?");
+        System.out.println("Hola, ¿Qué tal? ¿Me has escuchado?");
     }
 }
