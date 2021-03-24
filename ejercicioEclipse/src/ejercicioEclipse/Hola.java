9fc04e420bffdd4ac5d6753361d39b417c745054
fpeguero58 <franpeguerogarcia@gmail.com>
2021-03-24 17:27:07 +0100

    Primer commit

--- ejercicioEclipse/src/ejercicioEclipse/Hola.java
+++ ejercicioEclipse/src/ejercicioEclipse/Hola.java
@@ -2,8 +2,6 @@ package ejercicioEclipse;
 
 public class Hola {
     public static void main(String[] args) {
-
-        System.out.println("Hola bro");
-
+        System.out.println("Holaaaaaaaaaaaaa");
     }
 }
